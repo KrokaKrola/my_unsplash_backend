1070441f4fc702629217d5c448daba1385b457e5
Frontend Oleh Kotlinskyi <oleh.kotlinskyi@abz.agency>
2022-04-08 00:55:44 +0300

    feat: delete postgres configuration and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from './common/logger/Logger.module';
 import { AppConfigModule } from './config/app/configuration.module';
-import { PostgresConfigModule } from './config/database/postgres/configuration/configuration.module';
 import { RedisConfigModule } from './config/database/redis/configuration.module';
-import { ProviderModule as PostgresProvider } from './config/database/postgres/provider/provider.module';
 import { UsersModule } from './modules/users/users.module';
 import { ApiConfigurationModule } from './config/api/configuration.module';
 import { MailerModule } from './modules/mailer/mailer.module';
@@ -19,12 +17,10 @@ import { PrismaService } from './prisma.service';
   imports: [
     LoggerModule,
     AppConfigModule,
-    PostgresConfigModule,
     RedisConfigModule,
     ApiConfigurationModule,
     EmailsConfigurationModule,
     JwtConfigurationModule,
-    PostgresProvider,
     RedisProviderModule,
     MailerModule,
     UsersModule,
